54d226a20a36949527bc18f6cb8f8c7c2ee932a2
eric-kitagawa <kitagawa.e@northeastern.edu>
2026-04-22 17:02:25 -0400

    fix: less cringe intro

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -2,9 +2,11 @@ export default function Intro() {
     return (
         <section className="mx-auto flex w-[50%] flex-col py-10">
             <h1 className="text-2xl leading-[0.95] tracking-tight sm:text-7xl md:text-8xl lg:text-5xl leading-snug">
-                Eric Kitagawa is a CS student at NU.
+                Eric Kitagawa is a software engineer.
                 <br />
-                Currently building and fencing in Boston.
+                I study CS at Northeastern.
+                <br />
+                I code and fence a lot.
             </h1>
         </section>
     );

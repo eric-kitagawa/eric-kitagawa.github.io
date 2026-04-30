35533b4c3f4caf11b7af8f19dc876e3791dac0c1
eric-kitagawa <kitagawa.e@northeastern.edu>
2026-04-30 17:36:40 -0400

    fix: match title font to rest of page

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,7 +1,7 @@
 export default function Intro() {
     return (
         <section className="mx-auto flex w-[50%] flex-col py-10">
-            <h1 className="text-2xl leading-[0.95] tracking-tight sm:text-7xl md:text-8xl lg:text-5xl leading-snug">
+            <h1 className="font-mono text-2xl leading-snug tracking-tight sm:text-7xl md:text-8xl lg:text-4xl">
                 Eric Kitagawa is a CS student who
                 <br />
                 likes fencing and writing code.
